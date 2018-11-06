eb3825ed4ba2c3fc870a1c39ef1a8f1978bc65e1
chunghe <chunghe@gmail.com>
2018-11-06 09:10:40 +0800

    add PanGestureHandler

--- App.js
+++ App.js
@@ -1,18 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer, createDrawerNavigator } from 'react-navigation';
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer, createDrawerNavigator} from 'react-navigation';
+import {
+  PanGestureHandler,
+  TapGestureHandler,
+  ScrollView,
+  State,
+} from 'react-native-gesture-handler';
 
 class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <PanGestureHandler
+          onHandlerStateChange={e => {
+            console.log('onHandlerStateChange', e);
+          }}
+          onGestureEvent={e => {
+            console.log('onGestureEvent', e);
+          }}>
+          <View style={{height: 200, backgroundColor: 'tomato'}}>
+            <Text>PanGestureHandler</Text>
+          </View>
+        </PanGestureHandler>
       </View>
     );
   }
 }
 
-export default createAppContainer(createDrawerNavigator({ App }));
+export default createAppContainer(createDrawerNavigator({App}));
 
 const styles = StyleSheet.create({
   container: {
